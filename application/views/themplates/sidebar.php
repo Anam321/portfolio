e418509a9b853480f3eaa8882fc1cd74beaf9380
saepul anam <jackston9@gmail.com>
2022-05-01 01:59:52 +0700

    sfvsfvzsfvsfvfsv

--- application/views/themplates/sidebar.php
+++ application/views/themplates/sidebar.php
@@ -42,42 +42,20 @@
             <li class="side-nav-item">
                 <a href="<?= base_url('administrasi/profil') ?>" aria-expanded="false" aria-controls="sidebarEcommerce"
                     class="side-nav-link">
-                    <i class="mdi mdi-account-lock text-primary"></i>
+                    <i class="mdi mdi-account-lock "></i>
                     <span> Profil </span>
 
                 </a>
             </li>
-            <li class="side-nav-item">
-                <a data-bs-toggle="collapse" href="#sidebarEcommerce" aria-expanded="false"
-                    aria-controls="sidebarEcommerce" class="side-nav-link">
-                    <i class="mdi mdi-file-document-edit text-primary"></i>
-                    <span> Info Desa </span>
-                    <span class="menu-arrow"></span>
-                </a>
-                <div class="collapse" id="sidebarEcommerce">
-                    <ul class="side-nav-second-level">
-                        <li>
-                            <a href="<?= base_url('identitasDesa') ?>">Identitas Desa</a>
-                        </li>
-                        <li>
-                            <a href="<?= base_url('wilayahAdministrasi') ?>">Wilayah administrasi</a>
-                        </li>
-                        <li>
-                            <a href="apps-ecommerce-products-details.html">Pemerintahan Desa</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </li>
 
             <li class="side-nav-item">
-                <a data-bs-toggle="collapse" href="#sidebarEmail" aria-expanded="false" aria-controls="sidebarEmail"
+                <a data-bs-toggle="collapse" href="#forpolio" aria-expanded="false" aria-controls="forpolio"
                     class="side-nav-link">
-                    <i class="uil-envelope"></i>
-                    <span> Email </span>
+                    <i class="mdi mdi-bookmark-multiple-outline"></i>
+                    <span> Porfolio </span>
                     <span class="menu-arrow"></span>
                 </a>
-                <div class="collapse" id="sidebarEmail">
+                <div class="collapse" id="forpolio">
                     <ul class="side-nav-second-level">
                         <li>
                             <a href="apps-email-inbox.html">Inbox</a>
@@ -88,6 +66,50 @@
                     </ul>
                 </div>
             </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sastra" aria-expanded="false" aria-controls="sastra"
+                    class="side-nav-link">
+                    <i class="mdi mdi-book-open-blank-variant"></i>
+                    <span> Sastra </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sastra">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="apps-email-inbox.html">Inbox</a>
+                        </li>
+                        <li>
+                            <a href="apps-email-read.html">Read Email</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarEcommerce" aria-expanded="false"
+                    aria-controls="sidebarEcommerce" class="side-nav-link">
+                    <i class="mdi mdi-monitor-cellphone"></i>
+                    <span> Web </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarEcommerce">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?= base_url('identitasDesa') ?>">Artikel</a>
+                        </li>
+                        <li>
+                            <a href="<?= base_url('wilayahAdministrasi') ?>">Testimoni</a>
+                        </li>
+                        <li>
+                            <a href="apps-ecommerce-products-details.html">Contact</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+
 
         </ul>
 
